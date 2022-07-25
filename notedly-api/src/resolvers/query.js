b9a944ee2e9e248dfe82c7ae4c110798c5e4a008
Gagan <g.deepsingh1@gmail.com>
2022-07-25 11:07:07 -0600

    delete and update operations

--- notedly-api/src/resolvers/query.js
+++ notedly-api/src/resolvers/query.js
@@ -1,6 +1,7 @@
 module.exports = {
-    notes: () => async (parent, args, { models }) => {
-        return await models.Note.find();
+    notes: async (parent, args, { models }) => {
+        const notes = await models.Note.find();
+        return notes;
     },
     note: async (parent, args, { models }) => {
         return await models.Note.findById(args.id)
