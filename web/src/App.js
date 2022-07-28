6349607b8944cf0e74c0843844ad7d308b5debe2
Gagan <g.deepsingh1@gmail.com>
2022-07-28 16:48:53 -0600

    notes apis in ui

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import GlobalStyle from "./components/GlobalStyle";
+
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    uri,
+    cache,
+    headers: {
+        "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZGZmZjA5ZTI3NmQxOGQ0NWY3YTEzZiIsImlhdCI6MTY1ODg0Njk4NX0.pADUE_8LcdlCxb5awBTYS4BZV3wcby0n1y5zBb5jSbA"
+    },
+    connectToDevTools: true
+});
 
 const App = () => {
     return (
-        <div>
+        <ApolloProvider client={client}>
             <GlobalStyle />
             <Pages />
-        </div>
+        </ApolloProvider>
     )
 };
 
--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,9 +1,42 @@
 import React from "react";
-import Button from "../components/Button";
+import {gql, useQuery} from "@apollo/client";
+
+const GET_NOTES = gql`
+    query noteFeed($cursor: String!) {
+        noteFeed(cursor: $cursor) {
+            cursor
+            hasNextPage
+            notes {
+                id
+                createdAt
+                content
+                author {
+                    username
+                    id
+                    avatar
+                }
+            }
+        }
+    }
+`
 
 const Home = () => {
+    const { data, loading, error, fetchMore } = useQuery(GET_NOTES, {
+        variables: { cursor: "62e023fe53bf199d607afe16" }
+    });
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+    if (error) {
+        return <p>Error!</p>;
+    }
+
     return (
         <div>
+            {data.noteFeed.notes.map(note => (
+                <div key={note.id}>{note.author.username}</div>
+            ))}
             <p>This is the home page</p>
         </div>
     )
