61d4c4274ed363b7808c30a93ec7945c3cb3a98e
Gagan <g.deepsingh1@gmail.com>
2022-08-22 18:27:20 -0600

    sidebars and layouts change

---
+++ react-animation-portfolio/src/components/layout/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+import Sidebar from '../sidebar'
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+};
+
+export default Layout;
---
+++ react-animation-portfolio/src/components/sidebar/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+
+const Sidebar = () => {
+  return <>
+    Hello from sidebar
+  </>
+};
+
+export default Sidebar;
