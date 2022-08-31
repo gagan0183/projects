e780e0b1941620bfecd8d6a2c8b81e94388aa033
Gagan <g.deepsingh1@gmail.com>
2022-08-30 22:44:21 -0600

    home page animations

--- react-animation-portfolio/src/components/Home/index.js
+++ react-animation-portfolio/src/components/Home/index.js
@@ -1,7 +1,7 @@
 import LogoTitle from "../../assets/images/logo-s.png";
 import { Link } from 'react-router-dom'
 import "./index.scss";
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
@@ -9,13 +9,21 @@ const Home = () => {
   const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n'];
   const jobArray = ['w', 'e', 'b', '', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.'];
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 4000);
+  }, []);
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
         <span className={letterClass}>H</span>
         <span className={`${letterClass} _12`}>i</span>
-        <br /> I'm
+        <br />
+        <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
         <img src={LogoTitle} alt="developer"/>
         <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
         <br />
