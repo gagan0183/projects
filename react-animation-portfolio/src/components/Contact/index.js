f8ece4d8c3b345ea70ff15a8ace312e00738b3bb
Gagan <g.deepsingh1@gmail.com>
2022-09-05 19:06:24 -0600

    contacts

--- react-animation-portfolio/src/components/Contact/index.js
+++ react-animation-portfolio/src/components/Contact/index.js
@@ -1,16 +1,30 @@
 import './index.scss';
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
+  const refForm = useRef();
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass("text-animate-hover");
     }, 3000);
   }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('gmail', 'template_1w70avc', refForm.current, 'uTk838PWEFW-9fgCn').then(() => {
+      alert('Message succesffully sent!')
+      window.location.reload(false)
+    }, () => {
+      alert('Failed to send the message, please try again!');
+    })
+  }
 
   return (
     <>
@@ -21,7 +35,7 @@ const Contact = () => {
           </h1>
           <p>This is contacts</p>
           <div className='contact-form'>
-            <form>
+            <form ref={refForm} onSubmit={sendEmail}>
               <ul>
                 <li className='half'>
                   <input type='text' name='name' placeholder='Name' required/>
@@ -41,6 +55,23 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className='info-map'>
+          Gagan bhullar,
+          <br />
+          Canada
+          <br />
+          Edmonton
+          <br />
+          <span>g.deepsingh1@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[53.456584, -113.404595]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+            <Marker position={[53.456584, -113.404595]}>
+              <Popup>Gagan lives here</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type='pacman' />
