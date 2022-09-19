e1b9f5f5caa04367dc3fe3dab8c81905df2e9e01
Gagan <g.deepsingh1@gmail.com>
2022-09-18 21:31:32 -0600

    app

--- web/src/components/Note.js
+++ web/src/components/Note.js
@@ -1,18 +1,21 @@
-import React from "react";
-import ReactMarkdown from "react-markdown";
-import { format } from "date-fns";
-import styled from "styled-components";
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { format } from 'date-fns';
+import styled from 'styled-components';
+import NoteUser from './NoteUser';
+import { IS_LOGGED_IN } from '../gql/query';
+import { useQuery } from '@apollo/client';
 
 const StyledNote = styled.article`
-    max-width: 800px;
-    margin: 0 auto;
+  max-width: 800px;
+  margin: 0 auto;
 `;
 
 const MetaData = styled.div`
-    @media(min-width: 500px) {
-        display: flex;
-        align-items: top;
-    }
+  @media (min-width: 500px) {
+    display: flex;
+    align-items: top;
+  }
 `;
 
 const MetaInfo = styled.div`
@@ -25,23 +28,38 @@ const UserActions = styled.div`
 `;
 
 const Note = ({ note }) => {
-    return (
-        <StyledNote>
-            <MetaData>
-                <MetaInfo>
-                    <img src={note.author.avatar} alt={note.author.username + "avatar"} height="50px" />
-                </MetaInfo>
-                <MetaInfo>
-                    <em>by</em> {note.author.username} <br />
-                    {format(note.createdAt, 'MM Do YYYY')}
-                </MetaInfo>
-                <UserActions>
-                    <em>Favorites:</em> {note.favoriteCount}
-                </UserActions>
-            </MetaData>
-            <ReactMarkdown source={note.content} />
-        </StyledNote>
-    )
+  const { loading, error, data } = useQuery(IS_LOGGED_IN);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+
+  return (
+    <StyledNote>
+      <MetaData>
+        <MetaInfo>
+          <img
+            src={note.author.avatar}
+            alt={note.author.username + 'avatar'}
+            height="50px"
+          />
+        </MetaInfo>
+        <MetaInfo>
+          <em>by</em> {note.author.username} <br />
+          {format(note.createdAt, 'MM Do YYYY')}
+        </MetaInfo>
+        {data.isLoggedIn ? (
+          <UserActions>
+            <NoteUser note={note} />
+          </UserActions>
+        ) : (
+          <UserActions>
+            <em>Favorites:</em> {note.favoriteCount}
+          </UserActions>
+        )}
+      </MetaData>
+      <ReactMarkdown source={note.content} />
+    </StyledNote>
+  );
 };
 
 export default Note;
--- web/src/pages/edit.js
+++ web/src/pages/edit.js
@@ -2,17 +2,27 @@ import React from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 import NoteForm from '../components/NoteForm';
 import { GET_NOTE } from '../gql/query';
+import { EDIT_NOTE } from '../gql/mutation';
 
 const EditNote = props => {
   const id = props.match.params.id;
   const { loading, error, data } = useQuery(GET_NOTE, { variables: { id } });
+  const { data: userdata } = useQuery(GET_NOTE);
+  const [editNote] = useMutation(EDIT_NOTE, {
+    variables: {
+      id
+    },
+    onCompleted: () => {
+      props.history.push(`/note/${id}`);
+    }
+  });
 
   if (loading) return 'Loading...';
   if (error) return <p>Error! Note not found</p>;
   if (userdata.me.id !== data.note.author.id) {
     return <p>You do not have access to edit this note</p>;
   }
-  return <NoteForm content={data.note.content}></NoteForm>;
+  return <NoteForm content={data.note.content} action={editNote}></NoteForm>;
 };
 
 export default EditNote;
